e346f4285d1cd82efc6b997bc43e568881160591
Vicente Martínez <vicente@iesdoctorbalmis.com>
2022-10-23 23:06:34 +0200

    v2022.3.2 Unit 3 corrections and 2nd part published

---
+++ assets/test_page.html.5d1d215f.js
@@ -0,0 +1,32 @@
+import{_ as p,r as t,o as d,c as _,d as o,a as e,w as l,e as u,b as n,f as r}from"./app.b7fd2ce2.js";const h={},m=e("div",{class:"pagebreak"},null,-1),g=e("h1",{id:"psp",tabindex:"-1"},"PSP",-1),v={class:"table-of-contents"},A=n("Acerca de"),f=n("Segundo apartado"),b=n("Segundo apartado"),y=e("h2",{id:"acerca-de",tabindex:"-1"},"Acerca de",-1),w={class:"custom-container info"},E=e("p",{class:"custom-container-title"},"INFO",-1),k={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},S=n('"Google"'),D={class:"custom-container tip"},C=e("p",{class:"custom-container-title"},"Aviso",-1),B={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},P=n('"Google"'),R={class:"custom-container warning"},x=e("p",{class:"custom-container-title"},"Aviso",-1),F={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},O=n('"Google"'),G={class:"custom-container danger"},N=e("p",{class:"custom-container-title"},"Aviso",-1),j={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},q=n('"Google"'),I=r(`<div class="custom-container question"><p class="custom-container-title">Chech your skills</p><p>\xBFQu\xE9 pasar\xE1 ahora?</p></div><div class="pagebreak"></div><h2 id="segundo-apartado" tabindex="-1">Segundo apartado</h2><div class="language-java ext-java line-numbers-mode"><pre class="shiki" style="background-color:#FAFAFA;"><code><span class="line"><span style="color:#9C3EDA;">public</span><span style="color:#90A4AE;"> </span><span style="color:#9C3EDA;">static</span><span style="color:#90A4AE;"> </span><span style="color:#9C3EDA;">void</span><span style="color:#90A4AE;"> </span><span style="color:#6182B8;">main</span><span style="color:#39ADB5;">()</span><span style="color:#90A4AE;"> </span><span style="color:#39ADB5;">{</span></span>
+<span class="line"><span style="color:#90A4AE;">    </span><span style="color:#39ADB5;font-style:italic;">return</span><span style="color:#39ADB5;">;</span></span>
+<span class="line"><span style="color:#39ADB5;">}</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),U={class:"custom-container info"},V=e("p",{class:"custom-container-title"},"INFO",-1),K={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},L=n('"Google"'),Q={class:"custom-container tip"},z=e("p",{class:"custom-container-title"},"Aviso",-1),T={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},H=n('"Google"'),J={class:"custom-container warning"},M=e("p",{class:"custom-container-title"},"Aviso",-1),W={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},X=n('"Google"'),Y={class:"custom-container danger"},Z=e("p",{class:"custom-container-title"},"Aviso",-1),$={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},ee=n('"Google"'),oe=r(`<div class="custom-container question"><p class="custom-container-title">Chech your skills</p><p>\xBFQu\xE9 pasar\xE1 ahora?</p></div><div class="pagebreak"></div><h2 id="segundo-apartado-1" tabindex="-1">Segundo apartado</h2><div class="language-java ext-java line-numbers-mode"><pre class="shiki" style="background-color:#FAFAFA;"><code><span class="line"><span style="color:#9C3EDA;">public</span><span style="color:#90A4AE;"> </span><span style="color:#9C3EDA;">static</span><span style="color:#90A4AE;"> </span><span style="color:#9C3EDA;">void</span><span style="color:#90A4AE;"> </span><span style="color:#6182B8;">main</span><span style="color:#39ADB5;">()</span><span style="color:#90A4AE;"> </span><span style="color:#39ADB5;">{</span></span>
+<span class="line"><span style="color:#90A4AE;">    </span><span style="color:#39ADB5;font-style:italic;">return</span><span style="color:#39ADB5;">;</span></span>
+<span class="line"><span style="color:#39ADB5;">}</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="pagebreak"></div>`,5);function ne(se,te){const c=t("DownloadPDF-component"),i=t("DocumentCover-component"),a=t("router-link"),s=t("ExternalLinkIcon");return d(),_("div",null,[m,o(c),o(i,{titulo:"PSP"}),g,e("nav",v,[e("ul",null,[e("li",null,[o(a,{to:"#acerca-de"},{default:l(()=>[A]),_:1})]),e("li",null,[o(a,{to:"#segundo-apartado"},{default:l(()=>[f]),_:1})]),e("li",null,[o(a,{to:"#segundo-apartado-1"},{default:l(()=>[b]),_:1})])])]),y,e("div",w,[E,e("p",null,[e("a",k,[S,o(s)])])]),e("div",D,[C,e("p",null,[e("a",B,[P,o(s)])])]),e("div",R,[x,e("p",null,[e("a",F,[O,o(s)])])]),e("div",G,[N,e("p",null,[e("a",j,[q,o(s)])])]),I,e("div",U,[V,e("p",null,[e("a",K,[L,o(s)])])]),e("div",Q,[z,e("p",null,[e("a",T,[H,o(s)])])]),e("div",J,[M,e("p",null,[e("a",W,[X,o(s)])])]),e("div",Y,[Z,e("p",null,[e("a",$,[ee,o(s)])])]),oe,u(`
+## Diagrama de flujo
+![diagrama de sequencia|, 20%](/media/puml_sequence_test.png)
+
+\`\`\`puml {align="center", style="zoom:1"}
+@startuml
+skinparam handwritten true
+title Protocolo validaci\xF3n user/passwd
+header Prueba
+footer Prueba
+hide footbox
+skinparam sequence {
+ParticipantBorderColor DeepSkyBlue
+ParticipantBackgroundColor DodgerBlue
+} 
+note left of Cliente: El programa cliente lee por teclado\\nun usuario y una contrase\xF1a 
+Cliente->Servidor: Usuario
+Cliente->Servidor: Password
+note right of Servidor: El programa servidor comprueba\\n- Si el usuario es "vicente"\\n     contesta "USEROK" \\nsino \\n    contesta "USERERROR"
+note right of Servidor: El programa servidor comprueba\\n- Si el password es "psp"\\n     contesta "PASSOK" \\nsino \\n    contesta "PASSERROR"
+Servidor->Cliente: USEROK \xF3 USERERROR
+Servidor->Cliente: PASSOK \xF3 PASSERROR
+note left of Cliente: El programa mostrar\xE1 un mensaje\\nen base a la informaci\xF3n recibida\\nIndicando si el usuario y la contrase\xF1a\\nenviadas han diso correctas o no
+@enduml
+\`\`\`
+`)])}const le=p(h,[["render",ne],["__file","test_page.html.vue"]]);export{le as default};
