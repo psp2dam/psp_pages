506fa1274d6ca1c92d1e80d4cb84514b6f2016c2
Vicente Martínez <vicente@iesdoctorbalmis.com>
2024-10-04 18:51:32 +0200

    v2024.3.1 Unit3 update

---
+++ assets/test_page.html-34e19b2a.js
@@ -0,0 +1,32 @@
+import{_ as p,r as a,o as d,c as u,d as n,a as e,w as l,b as o,e as _,f as r}from"./app-9b4200bb.js";const h={},m=e("div",{class:"pagebreak"},null,-1),g=e("h1",{id:"psp",tabindex:"-1"},"PSP",-1),v={class:"table-of-contents"},A=e("h2",{id:"acerca-de",tabindex:"-1"},"Acerca de",-1),f={class:"custom-container info"},b=e("p",{class:"custom-container-title"},"INFO",-1),y={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},w={class:"custom-container tip"},E=e("p",{class:"custom-container-title"},"Aviso",-1),k={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},S={class:"custom-container warning"},D=e("p",{class:"custom-container-title"},"Aviso",-1),C={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},B={class:"custom-container danger"},x=e("p",{class:"custom-container-title"},"Aviso",-1),P={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},R=r(`<div class="custom-container question"><p class="custom-container-title">Chech your skills</p><p>¿Qué pasará ahora?</p></div><div class="pagebreak"></div><h2 id="segundo-apartado" tabindex="-1">Segundo apartado</h2><div class="language-java line-numbers-mode" data-ext="java"><pre class="shiki material-theme-lighter" style="background-color:#FAFAFA;" tabindex="0"><code><span class="line"><span style="color:#9C3EDA;">public</span><span style="color:#90A4AE;"> </span><span style="color:#9C3EDA;">static</span><span style="color:#90A4AE;"> </span><span style="color:#9C3EDA;">void</span><span style="color:#90A4AE;"> </span><span style="color:#6182B8;">main</span><span style="color:#39ADB5;">()</span><span style="color:#90A4AE;"> </span><span style="color:#39ADB5;">{</span></span>
+<span class="line"><span style="color:#90A4AE;">    </span><span style="color:#39ADB5;font-style:italic;">return</span><span style="color:#39ADB5;">;</span></span>
+<span class="line"><span style="color:#39ADB5;">}</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),F={class:"custom-container info"},O=e("p",{class:"custom-container-title"},"INFO",-1),G={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},N={class:"custom-container tip"},j=e("p",{class:"custom-container-title"},"Aviso",-1),q={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},I={class:"custom-container warning"},U=e("p",{class:"custom-container-title"},"Aviso",-1),V={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},K={class:"custom-container danger"},L=e("p",{class:"custom-container-title"},"Aviso",-1),Q={href:"http://www.google.es",target:"_blank",rel:"noopener noreferrer"},z=r(`<div class="custom-container question"><p class="custom-container-title">Chech your skills</p><p>¿Qué pasará ahora?</p></div><div class="pagebreak"></div><h2 id="segundo-apartado-1" tabindex="-1">Segundo apartado</h2><div class="language-java line-numbers-mode" data-ext="java"><pre class="shiki material-theme-lighter" style="background-color:#FAFAFA;" tabindex="0"><code><span class="line"><span style="color:#9C3EDA;">public</span><span style="color:#90A4AE;"> </span><span style="color:#9C3EDA;">static</span><span style="color:#90A4AE;"> </span><span style="color:#9C3EDA;">void</span><span style="color:#90A4AE;"> </span><span style="color:#6182B8;">main</span><span style="color:#39ADB5;">()</span><span style="color:#90A4AE;"> </span><span style="color:#39ADB5;">{</span></span>
+<span class="line"><span style="color:#90A4AE;">    </span><span style="color:#39ADB5;font-style:italic;">return</span><span style="color:#39ADB5;">;</span></span>
+<span class="line"><span style="color:#39ADB5;">}</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="pagebreak"></div>`,5);function T(H,J){const i=a("DownloadPDF-component"),c=a("DocumentCover-component"),t=a("router-link"),s=a("ExternalLinkIcon");return d(),u("div",null,[m,n(i),n(c,{titulo:"PSP"}),g,e("nav",v,[e("ul",null,[e("li",null,[n(t,{to:"#acerca-de"},{default:l(()=>[o("Acerca de")]),_:1})]),e("li",null,[n(t,{to:"#segundo-apartado"},{default:l(()=>[o("Segundo apartado")]),_:1})]),e("li",null,[n(t,{to:"#segundo-apartado-1"},{default:l(()=>[o("Segundo apartado")]),_:1})])])]),A,e("div",f,[b,e("p",null,[e("a",y,[o('"Google"'),n(s)])])]),e("div",w,[E,e("p",null,[e("a",k,[o('"Google"'),n(s)])])]),e("div",S,[D,e("p",null,[e("a",C,[o('"Google"'),n(s)])])]),e("div",B,[x,e("p",null,[e("a",P,[o('"Google"'),n(s)])])]),R,e("div",F,[O,e("p",null,[e("a",G,[o('"Google"'),n(s)])])]),e("div",N,[j,e("p",null,[e("a",q,[o('"Google"'),n(s)])])]),e("div",I,[U,e("p",null,[e("a",V,[o('"Google"'),n(s)])])]),e("div",K,[L,e("p",null,[e("a",Q,[o('"Google"'),n(s)])])]),z,_(`
+## Diagrama de flujo
+![diagrama de sequencia|, 20%](/media/puml_sequence_test.png)
+
+\`\`\`puml {align="center", style="zoom:1"}
+@startuml
+skinparam handwritten true
+title Protocolo validación user/passwd
+header Prueba
+footer Prueba
+hide footbox
+skinparam sequence {
+ParticipantBorderColor DeepSkyBlue
+ParticipantBackgroundColor DodgerBlue
+} 
+note left of Cliente: El programa cliente lee por teclado\\nun usuario y una contraseña 
+Cliente->Servidor: Usuario
+Cliente->Servidor: Password
+note right of Servidor: El programa servidor comprueba\\n- Si el usuario es "vicente"\\n     contesta "USEROK" \\nsino \\n    contesta "USERERROR"
+note right of Servidor: El programa servidor comprueba\\n- Si el password es "psp"\\n     contesta "PASSOK" \\nsino \\n    contesta "PASSERROR"
+Servidor->Cliente: USEROK ó USERERROR
+Servidor->Cliente: PASSOK ó PASSERROR
+note left of Cliente: El programa mostrará un mensaje\\nen base a la información recibida\\nIndicando si el usuario y la contraseña\\nenviadas han diso correctas o no
+@enduml
+\`\`\`
+`)])}const W=p(h,[["render",T],["__file","test_page.html.vue"]]);export{W as default};
